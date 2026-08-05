aa1c6a3a2366a3aca65bb128532ed59330f3380d
Bernardo <behornerl@gmail.com>
2026-08-05 09:48:54 -0300

    MatchModel.cs - Add MatchBonusType, MatchedTiles and Priority fields

--- Assets/Project/Script/Models/MatchModels/MatchModel.cs
+++ Assets/Project/Script/Models/MatchModels/MatchModel.cs
@@ -1,14 +1,14 @@
 using System.Collections.Generic;
-using Gazeus.DesafioMatch3.Controllers.MatchControllers;
 using UnityEngine;
 
 namespace Gazeus.DesafioMatch3.Models
 {
     public class MatchModel
     {
-        public List<Tile> MatchedTiles { get; set; }
+        public int Priority { get; set; }
+        public List<Vector2Int> MatchedTiles { get; set; }
         public MatchShape Shape { get; set; }
         public Vector2Int CenterPosition { get; set; }
-        public IMatchPatternStrategy SourcePatternStrategy { get; set; }
+        public MatchBonusType MatchBonusType { get; set; }
     }
 }
